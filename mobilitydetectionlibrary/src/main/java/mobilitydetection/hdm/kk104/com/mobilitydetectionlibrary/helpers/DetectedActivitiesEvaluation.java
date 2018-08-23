6b9a20c0a95491fc27812e79dd6d672f20e10dce
Kaan Keskinsoy <kk104@hdm-stuttgart.de>
2018-08-23 12:44:04 +0200

    Remove WALKING parameter for activity evaluation.

--- mobilitydetectionlibrary/src/main/java/mobilitydetection/hdm/kk104/com/mobilitydetectionlibrary/helpers/DetectedActivitiesEvaluation.java
+++ mobilitydetectionlibrary/src/main/java/mobilitydetection/hdm/kk104/com/mobilitydetectionlibrary/helpers/DetectedActivitiesEvaluation.java
@@ -45,44 +45,44 @@ public class DetectedActivitiesEvaluation {
             return (probableActivities.IN_VEHICLE >= InVehicle.average - InVehicle.mean_deviation / 2 // 17.2
                     && probableActivities.IN_VEHICLE <= InVehicle.average + InVehicle.mean_deviation / 2 // 30.2
                     && probableActivities.STILL >= Still.average - Still.mean_deviation / 2 // 14
-                    && probableActivities.STILL <= Still.average + Still.mean_deviation / 2 // 25.6
-                    && probableActivities.WALKING >= Walking.average - Walking.mean_deviation / 2
-                    && probableActivities.WALKING <= Walking.average + Walking.mean_deviation / 2)
+                    && probableActivities.STILL <= Still.average + Still.mean_deviation / 2) // 25.6
+                    /*&& probableActivities.WALKING >= Walking.average - Walking.mean_deviation / 2
+                    && probableActivities.WALKING <= Walking.average + Walking.mean_deviation / 2)*/
 
                     || (probableActivities.IN_VEHICLE >= InVehicle.average - InVehicle.std_deviation / 2 // 15.7
                     && probableActivities.IN_VEHICLE <= InVehicle.average + InVehicle.std_deviation / 2 // 31.7
                     && probableActivities.STILL >= Still.average - Still.std_deviation / 2 // 12.65
-                    && probableActivities.STILL <= Still.average + Still.std_deviation / 2 // 26.95
-                    && probableActivities.WALKING >= Walking.average - Walking.std_deviation / 2
-                    && probableActivities.WALKING <= Walking.average + Walking.std_deviation / 2)
+                    && probableActivities.STILL <= Still.average + Still.std_deviation / 2) // 26.95
+                    /*&& probableActivities.WALKING >= Walking.average - Walking.std_deviation / 2
+                    && probableActivities.WALKING <= Walking.average + Walking.std_deviation / 2)*/
 
                     || (probableActivities.IN_VEHICLE >= InVehicle.quartile_distance - InVehicle.median_deviation / 2 // 15.5
                     && probableActivities.IN_VEHICLE <= InVehicle.quartile_distance + InVehicle.median_deviation / 2 // 24.5
                     && probableActivities.STILL >= Still.quartile_distance - Still.median_deviation / 2 // 15.5
-                    && probableActivities.STILL <= Still.quartile_distance + Still.median_deviation / 2 // 22.5
-                    && probableActivities.WALKING >= Walking.quartile_distance - Walking.mean_deviation / 2
-                    && probableActivities.WALKING <= Walking.quartile_distance + Walking.mean_deviation / 2)
+                    && probableActivities.STILL <= Still.quartile_distance + Still.median_deviation / 2) // 22.5
+                    /*&& probableActivities.WALKING >= Walking.quartile_distance - Walking.mean_deviation / 2
+                    && probableActivities.WALKING <= Walking.quartile_distance + Walking.mean_deviation / 2)*/
 
                     || (probableActivities.IN_VEHICLE >= InVehicle.median - InVehicle.median_deviation / 2 // 14.5
                     && probableActivities.IN_VEHICLE <= InVehicle.median + InVehicle.median_deviation / 2 // 23.5
                     && probableActivities.STILL >= Still.median - Still.median_deviation / 2 // 11.5
-                    && probableActivities.STILL <= Still.median + Still.median_deviation / 2 // 18.5
-                    && probableActivities.WALKING >= Walking.median - Walking.median_deviation / 2
-                    && probableActivities.WALKING <= Walking.median + Walking.median_deviation / 2)
+                    && probableActivities.STILL <= Still.median + Still.median_deviation / 2) // 18.5
+                    /*&& probableActivities.WALKING >= Walking.median - Walking.median_deviation / 2
+                    && probableActivities.WALKING <= Walking.median + Walking.median_deviation / 2)*/
 
                     || (probableActivities.IN_VEHICLE >= InVehicle.median - InVehicle.semi_quartile_distance / 2 // 14
                     && probableActivities.IN_VEHICLE <= InVehicle.median + InVehicle.semi_quartile_distance / 2 // 24
                     && probableActivities.STILL >= Still.median - Still.semi_quartile_distance / 2 // 10.25
-                    && probableActivities.STILL <= Still.median + Still.semi_quartile_distance / 2 // 19.75
-                    && probableActivities.WALKING >= Walking.median - Walking.semi_quartile_distance / 2
-                    && probableActivities.WALKING <= Walking.median + Walking.semi_quartile_distance / 2)
+                    && probableActivities.STILL <= Still.median + Still.semi_quartile_distance / 2) // 19.75
+                    /*&& probableActivities.WALKING >= Walking.median - Walking.semi_quartile_distance / 2
+                    && probableActivities.WALKING <= Walking.median + Walking.semi_quartile_distance / 2)*/
 
                     || (probableActivities.IN_VEHICLE >= InVehicle.quartile_distance - InVehicle.mean_deviation / 2 // 13.5
                     && probableActivities.IN_VEHICLE <= InVehicle.quartile_distance + InVehicle.mean_deviation / 2 // 26.5
                     && probableActivities.STILL >= Still.quartile_distance - Still.mean_deviation / 2 // 13.2
-                    && probableActivities.STILL <= Still.quartile_distance + Still.mean_deviation / 2 // 24.8
-                    && probableActivities.WALKING >= Walking.quartile_distance - Walking.mean_deviation / 2
-                    && probableActivities.WALKING <= Walking.quartile_distance + Walking.mean_deviation / 2);
+                    && probableActivities.STILL <= Still.quartile_distance + Still.mean_deviation / 2); // 24.8
+                    /*&& probableActivities.WALKING >= Walking.quartile_distance - Walking.mean_deviation / 2
+                    && probableActivities.WALKING <= Walking.quartile_distance + Walking.mean_deviation / 2);*/
         }
     }
 
@@ -127,44 +127,44 @@ public class DetectedActivitiesEvaluation {
             return (probableActivities.IN_VEHICLE >= InVehicle.quartile_distance - InVehicle.median_deviation / 2 // 46
                     && probableActivities.IN_VEHICLE <= InVehicle.quartile_distance + InVehicle.median_deviation / 2 // 66
                     && probableActivities.STILL >= Still.quartile_distance - Still.median_deviation / 2 // 15.5
-                    && probableActivities.STILL <= Still.quartile_distance + Still.median_deviation / 2 // 29
-                    && probableActivities.WALKING >= Walking.quartile_distance - Walking.median_deviation / 2
-                    && probableActivities.WALKING <= Walking.quartile_distance + Walking.median_deviation / 2)
+                    && probableActivities.STILL <= Still.quartile_distance + Still.median_deviation / 2) // 29
+                    /*&& probableActivities.WALKING >= Walking.quartile_distance - Walking.median_deviation / 2
+                    && probableActivities.WALKING <= Walking.quartile_distance + Walking.median_deviation / 2)*/
 
                     || (probableActivities.IN_VEHICLE >= InVehicle.quartile_distance - InVehicle.mean_deviation / 2 // 42.5
                     && probableActivities.IN_VEHICLE <= InVehicle.quartile_distance + InVehicle.mean_deviation / 2 // 69.5
                     && probableActivities.STILL >= Still.quartile_distance - Still.mean_deviation / 2 // 13
-                    && probableActivities.STILL <= Still.quartile_distance + Still.mean_deviation / 2 // 27
-                    && probableActivities.WALKING >= Walking.quartile_distance - Walking.mean_deviation / 2
-                    && probableActivities.WALKING <= Walking.quartile_distance + Walking.mean_deviation / 2)
+                    && probableActivities.STILL <= Still.quartile_distance + Still.mean_deviation / 2) // 27
+                    /*&& probableActivities.WALKING >= Walking.quartile_distance - Walking.mean_deviation / 2
+                    && probableActivities.WALKING <= Walking.quartile_distance + Walking.mean_deviation / 2)*/
 
                     || (probableActivities.IN_VEHICLE >= InVehicle.average - InVehicle.mean_deviation / 2 // 27.1
                     && probableActivities.IN_VEHICLE <= InVehicle.average + InVehicle.mean_deviation / 2 // 54.1
                     && probableActivities.STILL >= Still.average - Still.mean_deviation / 2 // 9.5
-                    && probableActivities.STILL <= Still.average + Still.mean_deviation / 2 // 23.5
-                    && probableActivities.WALKING >= Walking.average - Walking.mean_deviation / 2
-                    && probableActivities.WALKING <= Walking.average + Walking.mean_deviation / 2)
+                    && probableActivities.STILL <= Still.average + Still.mean_deviation / 2) // 23.5
+                    /*&& probableActivities.WALKING >= Walking.average - Walking.mean_deviation / 2
+                    && probableActivities.WALKING <= Walking.average + Walking.mean_deviation / 2)*/
 
                     || (probableActivities.IN_VEHICLE >= InVehicle.average - InVehicle.std_deviation / 2 // 25.3
                     && probableActivities.IN_VEHICLE <= InVehicle.average + InVehicle.std_deviation / 2 // 55.9
                     && probableActivities.STILL >= Still.average - Still.std_deviation / 2 // 7.45
-                    && probableActivities.STILL <= Still.average + Still.std_deviation / 2 // 22.55
-                    && probableActivities.WALKING >= Walking.average - Walking.std_deviation / 2
-                    && probableActivities.WALKING <= Walking.average + Walking.std_deviation / 2)
+                    && probableActivities.STILL <= Still.average + Still.std_deviation / 2) // 22.55
+                    /*&& probableActivities.WALKING >= Walking.average - Walking.std_deviation / 2
+                    && probableActivities.WALKING <= Walking.average + Walking.std_deviation / 2)*/
 
                     || (probableActivities.IN_VEHICLE >= InVehicle.median - InVehicle.median_deviation / 2 // 20
                     && probableActivities.IN_VEHICLE <= InVehicle.median + InVehicle.median_deviation / 2 // 40
                     && probableActivities.STILL >= Still.median - Still.median_deviation / 2 // 5.5
-                    && probableActivities.STILL <= Still.median + Still.median_deviation / 2 // 14.5
-                    && probableActivities.WALKING >= Walking.median - Walking.median_deviation / 2
-                    && probableActivities.WALKING <= Walking.median + Walking.median_deviation / 2)
+                    && probableActivities.STILL <= Still.median + Still.median_deviation / 2) // 14.5
+                    /*&& probableActivities.WALKING >= Walking.median - Walking.median_deviation / 2
+                    && probableActivities.WALKING <= Walking.median + Walking.median_deviation / 2)*/
 
                     || (probableActivities.IN_VEHICLE >= InVehicle.median - InVehicle.semi_quartile_distance / 2 // 16
                     && probableActivities.IN_VEHICLE <= InVehicle.median + InVehicle.semi_quartile_distance / 2 // 44
                     && probableActivities.STILL >= Still.median - Still.semi_quartile_distance / 2 // 5
-                    && probableActivities.STILL <= Still.median + Still.semi_quartile_distance / 2 // 15
-                    && probableActivities.WALKING >= Walking.median - Walking.semi_quartile_distance / 2
-                    && probableActivities.WALKING <= Walking.median + Walking.semi_quartile_distance / 2);
+                    && probableActivities.STILL <= Still.median + Still.semi_quartile_distance / 2); // 15
+                    /*&& probableActivities.WALKING >= Walking.median - Walking.semi_quartile_distance / 2
+                    && probableActivities.WALKING <= Walking.median + Walking.semi_quartile_distance / 2);*/
         }
     }
 
@@ -209,44 +209,44 @@ public class DetectedActivitiesEvaluation {
             return (probableActivities.STILL >= Still.average - Still.mean_deviation / 2 // 21.25
                     && probableActivities.STILL <= Still.average + Still.mean_deviation / 2 // 34.15
                     && probableActivities.IN_VEHICLE >= InVehicle.average - InVehicle.mean_deviation / 2 // 14.95
-                    && probableActivities.IN_VEHICLE <= InVehicle.average + InVehicle.mean_deviation / 2 // 24.45
-                    && probableActivities.WALKING >= Walking.average - Walking.mean_deviation / 2
-                    && probableActivities.WALKING <= Walking.average + Walking.mean_deviation / 2)
+                    && probableActivities.IN_VEHICLE <= InVehicle.average + InVehicle.mean_deviation / 2) // 24.45
+                    /*&& probableActivities.WALKING >= Walking.average - Walking.mean_deviation / 2
+                    && probableActivities.WALKING <= Walking.average + Walking.mean_deviation / 2)*/
 
                     || (probableActivities.STILL >= Still.median - Still.median_deviation / 2 // 20
                     && probableActivities.STILL <= Still.median + Still.median_deviation / 2 // 30
                     && probableActivities.IN_VEHICLE >= InVehicle.median - InVehicle.median_deviation / 2 // 12.5
-                    && probableActivities.IN_VEHICLE <= InVehicle.median + InVehicle.median_deviation / 2 // 17.5
-                    && probableActivities.WALKING >= Walking.median - Walking.median_deviation / 2
-                    && probableActivities.WALKING <= Walking.median + Walking.median_deviation / 2)
+                    && probableActivities.IN_VEHICLE <= InVehicle.median + InVehicle.median_deviation / 2) // 17.5
+                    /*&& probableActivities.WALKING >= Walking.median - Walking.median_deviation / 2
+                    && probableActivities.WALKING <= Walking.median + Walking.median_deviation / 2)*/
 
                     || (probableActivities.STILL >= Still.average - Still.std_deviation / 2 // 19.55
                     && probableActivities.STILL <= Still.average + Still.std_deviation / 2 // 35.85
                     && probableActivities.IN_VEHICLE >= InVehicle.average - InVehicle.std_deviation / 2 // 13.5
-                    && probableActivities.IN_VEHICLE <= InVehicle.average + InVehicle.std_deviation / 2 // 25.9
-                    && probableActivities.WALKING >= Walking.average - Walking.std_deviation / 2
-                    && probableActivities.WALKING <= Walking.average + Walking.std_deviation / 2)
+                    && probableActivities.IN_VEHICLE <= InVehicle.average + InVehicle.std_deviation / 2) // 25.9
+                    /*&& probableActivities.WALKING >= Walking.average - Walking.std_deviation / 2
+                    && probableActivities.WALKING <= Walking.average + Walking.std_deviation / 2)*/
 
                     || (probableActivities.STILL >= Still.median - Still.semi_quartile_distance / 2 // 19.5
                     && probableActivities.STILL <= Still.median + Still.semi_quartile_distance / 2 // 30.5
                     && probableActivities.IN_VEHICLE >= InVehicle.median - InVehicle.semi_quartile_distance / 2 // 11.75
-                    && probableActivities.IN_VEHICLE <= InVehicle.median + InVehicle.semi_quartile_distance / 2 // 18.25
-                    && probableActivities.WALKING >= Walking.median - Walking.semi_quartile_distance / 2
-                    && probableActivities.WALKING <= Walking.median + Walking.semi_quartile_distance / 2)
+                    && probableActivities.IN_VEHICLE <= InVehicle.median + InVehicle.semi_quartile_distance / 2) // 18.25
+                    /*&& probableActivities.WALKING >= Walking.median - Walking.semi_quartile_distance / 2
+                    && probableActivities.WALKING <= Walking.median + Walking.semi_quartile_distance / 2)*/
 
                     || (probableActivities.STILL >= Still.quartile_distance - Still.median_deviation / 2 // 17
                     && probableActivities.STILL <= Still.quartile_distance + Still.median_deviation / 2 // 27
                     && probableActivities.IN_VEHICLE >= InVehicle.quartile_distance - InVehicle.median_deviation / 2 // 10.5
-                    && probableActivities.IN_VEHICLE <= InVehicle.quartile_distance + InVehicle.median_deviation / 2 // 15.5
-                    && probableActivities.WALKING >= Walking.quartile_distance - Walking.median_deviation / 2
-                    && probableActivities.WALKING <= Walking.quartile_distance + Walking.median_deviation / 2)
+                    && probableActivities.IN_VEHICLE <= InVehicle.quartile_distance + InVehicle.median_deviation / 2) // 15.5
+                    /*&& probableActivities.WALKING >= Walking.quartile_distance - Walking.median_deviation / 2
+                    && probableActivities.WALKING <= Walking.quartile_distance + Walking.median_deviation / 2)*/
 
                     || (probableActivities.STILL >= Still.quartile_distance - Still.mean_deviation / 2 // 15.55
                     && probableActivities.STILL <= Still.quartile_distance + Still.mean_deviation / 2 // 28.45
                     && probableActivities.IN_VEHICLE >= InVehicle.quartile_distance - InVehicle.mean_deviation / 2 // 8.25
-                    && probableActivities.IN_VEHICLE <= InVehicle.quartile_distance + InVehicle.mean_deviation / 2 // 17.75
-                    && probableActivities.WALKING >= Walking.quartile_distance - Walking.mean_deviation / 2
-                    && probableActivities.WALKING <= Walking.quartile_distance + Walking.mean_deviation / 2);
+                    && probableActivities.IN_VEHICLE <= InVehicle.quartile_distance + InVehicle.mean_deviation / 2); // 17.75
+                    /*&& probableActivities.WALKING >= Walking.quartile_distance - Walking.mean_deviation / 2
+                    && probableActivities.WALKING <= Walking.quartile_distance + Walking.mean_deviation / 2);*/
         }
     }
 }
